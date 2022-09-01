eefd931e5ea62c20765342028ed1359e71605359
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-31 23:38:12 -0700

    added comments with api key and stock symbol urls from twelvedata's documentation

--- public/index.js
+++ public/index.js
@@ -4,6 +4,20 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // api key: 4e62783a6f9b4b36ac43ae0f73d02656
+
+    // GME stock symbol request
+    // https://api.twelvedata.com/symbol_search?symbol=GME
+    
+    // MSFT stock symbol request
+    // https://api.twelvedata.com/symbol_search?symbol=MSFT
+
+    // DIS stock symbol request
+    // https://api.twelvedata.com/symbol_search?symbol=DIS
+
+    // BNTX stock symbol request
+    // https://api.twelvedata.com/symbol_search?symbol=BNTX
+
 }
 
 main()
